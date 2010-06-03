70c9bb1bbe2a44cef963b3cf7146eeff4d7f16bd
ciakana <ciakana@8c201bf4-8cbd-11de-a499-7ffba83fc0c5>
2010-06-03 09:51:04 +0000

    Finished adding support for http proxy. Added relevant settings in HashingPasswordFilter.ini
    Fixes issue 5

--- HashingPasswordFilter/HashingPasswordFilter.cpp
+++ HashingPasswordFilter/HashingPasswordFilter.cpp
@@ -54,10 +54,22 @@ bool loadConfig(){
     configuration.ldapAdminBindDn = loadSetting(path,L"ldapAdminBindDn",buffer,MAX_PATH + 1);
     configuration.ldapAdminPasswd = loadSetting(path,L"ldapAdminPasswd",buffer,MAX_PATH + 1);
     configuration.ldapSearchBaseDn = loadSetting(path,L"ldapSearchBaseDn",buffer,MAX_PATH + 1);
+    configuration.proxyAddress = loadSetting(path,L"proxyAddress",buffer,MAX_PATH + 1);
+    configuration.useProxy = lstrcmp(configuration.proxyAddress,L"")==0?false:true;
+    if (configuration.useProxy){
+        configuration.proxyUser = loadSetting(path,L"proxyUser",buffer,MAX_PATH + 1);
+        configuration.proxyPassword = loadSetting(path,L"proxyPassword",buffer,MAX_PATH + 1);
+    } else {
+        configuration.proxyUser = L"";
+        configuration.proxyPassword = L"";
+    }
     wchar_t* temp = loadSetting(path,L"processPath",buffer,MAX_PATH + 1);
-    int cmdLineLen = _scwprintf(PROCESS_COMMAND_LINE_FORMAT_STRING,temp,PROCESS_COMMAND_LINE_PARAMETERS) + 1;
+    int cmdLineLen = _scwprintf(PROCESS_COMMAND_LINE_FORMAT_STRING,temp,PROCESS_COMMAND_LINE_PARAMETERS,
+        configuration.proxyAddress, configuration.proxyUser, configuration.proxyPassword) + 1;
     configuration.processCommandLine = (wchar_t*)malloc(cmdLineLen * sizeof(wchar_t));
-    swprintf(configuration.processCommandLine,cmdLineLen,PROCESS_COMMAND_LINE_FORMAT_STRING,temp,PROCESS_COMMAND_LINE_PARAMETERS);
+    swprintf(configuration.processCommandLine,cmdLineLen,PROCESS_COMMAND_LINE_FORMAT_STRING,temp,
+        PROCESS_COMMAND_LINE_PARAMETERS, configuration.proxyAddress, configuration.proxyUser,
+        configuration.proxyPassword);
     free(temp);
     configuration.processPasswd = loadSetting(path,L"processPasswd",buffer,MAX_PATH + 1);
     configuration.processUser = loadSetting(path,L"processUser",buffer,MAX_PATH + 1);
